4a1f652b282a50627a10dc24c1a7b13da9216e8a
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2024-09-25 09:35:00 +0530

    basic implmentation of promises

--- async/promisesPolyfill.js
+++ async/promisesPolyfill.js
@@ -17,9 +17,63 @@ function samplePromise(a){
 // right now, on a super basic level, we've got the constructor and diff methods with it yeah. 
 
 class Promise {
-    constructor(execFn){
-        try{
-            
+    _fullFillmentTasks = []; 
+    _rejectionTasks = []; 
+    _promiseResult = undefined; 
+    _promiseState = 'pending';
+
+    then(onFulfilled, onRejected){
+        const fulFillmentTask = () => {
+            if(typeof onFulfilled === "function"){
+                onFulfilled(this._promiseResult); 
+            }
         }
+
+        const rejectionTask = () => {
+            if(typeof onRejected === "function"){
+                onRejected(this._promiseResult); 
+            }
+        }
+
+        switch(this._promiseResult){
+            case 'pending': 
+                this._fullFillmentTasks.push(fullFillmentTasks); 
+                this._rejectionTasks.push(rejectionTask);
+            case 'fulfilled':
+                addToTaskQueue(fulFillmentTask);
+                break; 
+            case 'rejected': 
+                addToTaskQueue(rejectionTask);
+                break; 
+            default: 
+                throw new Error('Invalid state');
+        }
+    }
+
+    resolve(value){
+        if(this._promiseState !== "pending") return this; 
+        this._promiseState = 'fulfilled'; 
+        this._promiseState = value; 
+        this._clearAndEnqueueTasks(this._fullFillmentTasks); 
+        return this; // enable chaining... 
+    }
+
+    reject(reason){
+        if(this._promiseState !== "pending") return this; 
+        this._promiseState = 'rejected'; 
+        this._promiseState = reason; 
+        this._clearAndEnqueueTasks(this._rejectionTasks); 
+        return this; // enable chaining... 
+    }
+
+    _clearAndEnqueueTasks(tasks){
+        this._fullFillmentTasks = undefined; 
+        this._rejectionTasks = undefined; 
+        tasks.map(addToTaskQueue); 
+    }
+
+
+    addToTaskQueue(task){
+        setTimeout(task,0); 
     }
 }
